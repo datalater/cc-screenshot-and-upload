dfb56216a64aacf5020f3d415712434e8a56ea1a
winner1231 <winner1231@gamil.com>
2024-02-03 11:23:44 +0900

    feat: add API Secret keys

--- lib/twitter-client.ts
+++ lib/twitter-client.ts
@@ -1,10 +1,10 @@
 import { TwitterApi } from "twitter-api-v2";
 
-const API_KEY = "";
-const API_SECRET = "";
-const ACCESS_TOKEN = "";
-const ACCESS_SECRET = "";
-const BEARER_TOKEN = "";
+const API_KEY = "NF83m7ccNb9qSI86xX5bTGDIJ";
+const API_SECRET = "DxGMVUawOcuQmGXy1RSZBgylXVlzfT9ujJMyya5mX33c0ez00y";
+const ACCESS_TOKEN = "1753590039047704576-FmgZ3vH6JSa1ShLIYsyJYc0hzQPcAb";
+const ACCESS_SECRET = "z6NpbXz508QjlIBERVs2JXdeAhBYECsCcDpXk9BmSsus0";
+const BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAAAzXsAEAAAAAVJJxdWGjh9SbSootSFJgfv01nKc%3DEA7EOWJPyTRwUnb6p36stw7YVZGVFk4g5VJL7sgAOYNtLfE5yx";
 
 const client = new TwitterApi({
   appKey: API_KEY,
